0bdb018f5c4e533b059965b90dd50736040b90b9
Ahmet Özkan <70511583+Ahmetzkan@users.noreply.github.com>
2023-12-23 21:14:02 +0300

    Add Global Ex Handling

--- Business/BusinessServiceRegistration.cs
+++ Business/BusinessServiceRegistration.cs
@@ -121,5 +121,23 @@ public static class BusinessServiceRegistration
         
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         return services;
+    
+    }
+
+    public static IServiceCollection AddSubClassesOfType(
+   this IServiceCollection services,
+   Assembly assembly,
+   Type type,
+   Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+
+            else
+                addWithLifeCycle(services, type);
+        return services;
     }
 }
